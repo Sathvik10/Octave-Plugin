77329f8fe20051c38170362f974ba26df8d363cf
Sathvik K R <42212825+Sathvik10@users.noreply.github.com>
2019-07-23 20:13:03 +0530

    Add files via upload

--- build_binary.sh
+++ build_binary.sh
@@ -1,26 +1,51 @@
 #!/bin/bash
 
-HPCC_CODE_DIR = $1
-
-if [-z "${HPCC_CODE_DIR}"];then
-    echo "Usage:$0<HPCC-Platform-Dir>"
-    exit
-fi
-
-if [!-d "${HPCC_CODE_DIR}"];then
-    echo "${HPCC_CODE_DIR} does not exist"
-    exit
-fi
-
-g++ -c std=c++11 -fPIC \
-    -l${HPCC_CODE_DIR}/system/include \
-    -l${HPCC_CODE_DIR}/system/jlib \
-    -l${HPCC_CODE_DIR}/rtl/eclrtl \
-    -l${HPCC_CODE_DIR}/rtl/include \
-    -l${HPCC_CODE_DIR}/common/deftype \
-    -o octaveplugin.o \
-    octavemebed.cpp
-
-g++ -shared octaveplugin.o -L/usr/local/lib/octave/5.1.0 -loctave -L/usr/local/lib/octave/5.1.0 -loctinterp -o liboctaveplugin.so
  
 
+HPCC_CODE_DIR=$1
+
+ 
+
+if [ -z "${HPCC_CODE_DIR}" ];then
+
+    echo "Usage: $0 <HPCC-Platform-Dir>"
+
+    exit
+
+fi
+
+ 
+
+if [ ! -d "${HPCC_CODE_DIR}" ];then
+
+    echo "${HPCC_CODE_DIR} does not exist"
+
+    exit
+
+fi
+
+ 
+
+g++ -c -std=c++11 -fPIC \
+
+    -I${HPCC_CODE_DIR}/system/include \
+
+    -I${HPCC_CODE_DIR}/system/jlib \
+
+    -I${HPCC_CODE_DIR}/rtl/eclrtl \
+
+    -I${HPCC_CODE_DIR}/rtl/include \
+
+    -I${HPCC_CODE_DIR}/common/deftype \
+
+    -I${HPCC_CODE_DIR}/roxie/roxiemem \
+
+    -I${HPCC_CODE_DIR}/rtl/nbcd \
+
+    -o octaveplugin.o \
+
+    octaveembed.cpp
+
+ 
+
+g++ -shared octaveplugin.o -L/usr/local/lib/octave/5.1.0 -loctave -Wl,-rpath /usr/local/lib/octave/5.1.0 -loctinterp -o liboctaveplugin.so
