1b71dec2e15953c062eff7f422f5abd1330f2350
Sathvik K R <42212825+Sathvik10@users.noreply.github.com>
2019-07-26 19:19:38 +0530

    Add files via upload

--- octaveembed.cpp
+++ octaveembed.cpp
@@ -1735,8 +1735,10 @@ public:
       if (!globalState)
       {
          globalState = new octave::interpreter;
+         parse_status = globalState->execute();
          threadHookChain = addThreadTermFunc(releaseContext);
       }
+      
       if (!globalState)
       {
          throw MakeStringException(-1, "%s", "Unable to initialize interpreter");
