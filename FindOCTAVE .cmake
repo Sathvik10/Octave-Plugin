ea58e06446d286b0f37ebc1ab7f4e0dbf2c2a6ed
Sathvik K R <42212825+Sathvik10@users.noreply.github.com>
2019-08-08 20:03:30 +0530

    Add files via upload

--- FindOCTAVE .cmake
+++ FindOCTAVE .cmake
@@ -1,14 +1,11 @@
 IF (NOT OCTAVE_FOUND)
-	FIND_LIBRARY (OCTAVE_LIBRARIES NAMES octave PATHS "/usr/local/lib/octave/5.1.0" NO_DEFAULT_PATH)
-	FIND_LIBRARY (OCTAVE_LIBRARIES2 NAMES octinterp PATHS "/usr/local/lib/octave/5.1.0" NO_DEFAULT_PATH)
-
-	if (USE_NATIVE_LIBRARIES)
-    		FIND_LIBRARY (OCTAVE_LIBRARIES NAMES ${octave_lib} PATH_SUFFIXES "octave")
-    		FIND_LIBRARY (OCTAVE_LIBRARIES NAMES ${octave_lib_interp} PATH_SUFFIXES "octave")
-  	endif()
+	FIND_PATH (OCTAVE_INCLUDE_DIR NAMES interpreter.h PATHS "/usr/local/include/octave-5.1.0/octave")
+	FIND_LIBRARY (OCTAVE_LIBRARIES_1 NAMES octave PATHS "/usr/local/lib/octave/5.1.0" NO_DEFAULT_PATH)
+	FIND_LIBRARY (OCTAVE_LIBRARIES_2 NAMES octinterp PATHS "/usr/local/lib/octave/5.1.0" NO_DEFAULT_PATH)
+	set (OCTAVE_LIBRARIES ${OCTAVE_LIBRARIES_1} ${OCTAVE_LIBRARIES_2})
 
 	include(FindPackageHandleStandardArgs)
-  	find_package_handle_standard_args(MYSQL DEFAULT_MSG
+  	find_package_handle_standard_args(OCTAVE DEFAULT_MSG
     	OCTAVE_LIBRARIES 
   	)
 
